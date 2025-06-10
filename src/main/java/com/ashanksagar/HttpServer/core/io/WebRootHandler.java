a46a23d3ec41015852f24718bb3838e90e697236
ashanksagar <ashanks3@illinois.edu>
2025-06-10 16:23:54 -0700

    POST/PUT Capabilities Cleanup

--- src/main/java/com/ashanksagar/HttpServer/core/io/WebRootHandler.java
+++ src/main/java/com/ashanksagar/HttpServer/core/io/WebRootHandler.java
@@ -21,12 +21,7 @@ public class WebRootHandler {
         return relativePath.endsWith("/");
     }
 
-    /**
-     * This method checks to see if the relative path provided exists inside WebRoot
-     *
-     * @param relativePath
-     * @return true if the path exists inside WebRoot, false if not.
-     */
+
     private boolean checkIfProvidedRelativePathExists(String relativePath) {
         File file = new File(webRoot, relativePath);
 
@@ -63,16 +58,7 @@ public class WebRootHandler {
         return mimeType;
     }
 
-    /**
-     * Returns a byte array of the content of a file.
-     *
-     * Todo - For large files a new strategy might be necessary.
-     *
-     * @param relativePath the path to the file inside the webroot folder.
-     * @return a byte array of the data.
-     * @throws FileNotFoundException if the file can not be found
-     * @throws ReadFileException if there was a problem reading the file.
-     */
+
     public byte[] getFileByteArrayData(String relativePath) throws FileNotFoundException, ReadFileException {
         if (checkIfEndsWithSlash(relativePath)) {
             relativePath += "index.html"; // By default serve the index.html, if it exists.
